e3184d0de61c96e8d3c292bc7485e92c1def9a08
mgodwillplus <matgoddy25@gmail.com>
2026-09-20 00:59:07 +0300

    Create catalogue.js

---
+++ catalogue.js
@@ -0,0 +1,720 @@
+/* =========================================================
+   MGODWILL+ STUDIO
+   CONTENT CATALOGUE
+   Version 1.0
+
+   This file contains the complete MGodwill+ media catalogue.
+   Do not edit the HTML when adding ordinary content.
+   ========================================================= */
+
+const MGODWILL_CATALOGUE = {
+
+    /* -----------------------------------------------------
+       SITE INFORMATION
+       ----------------------------------------------------- */
+
+    site: {
+        name: "MGODWILL+",
+        studio: "STUDIO",
+        blog: "https://gmspecrum.blogspot.com/?m=1",
+
+        aboutPhoto:
+            "https://drive.google.com/file/d/1YmAM1R7gPEcmafseI0KOOqhoNZMarXgU/view?usp=drivesdk",
+
+        mavunoChurchImage:
+            "https://drive.google.com/file/d/1KnTQ3XsrNH-x97WH2TxurazwIS4oEqSz/view?usp=drivesdk"
+    },
+
+
+    /* -----------------------------------------------------
+       VIDEOS
+       ----------------------------------------------------- */
+
+    videos: [
+
+        {
+            id: "first-animation",
+            title: "First Animation",
+            type: "Video",
+            category: "Renders",
+
+            media:
+                "https://drive.google.com/file/d/1Jq6XlM3jAuO6Y1Vyj11mqK7gFmiOXxt4/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1FZC1TGkK-qzyMbc1sTspCOcg2MmtH-KZ/view?usp=drivesdk",
+
+            description:
+                "This video was made with Prisma 3d"
+        },
+
+
+        {
+            id: "happy-sunday",
+            title: "Happy Sunday",
+            type: "Video",
+            category: "Mavuno Church",
+
+            media:
+                "https://drive.google.com/file/d/1KMxS4jqz_7FswzisrdV4Qt3y5aN8eUTO/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1w6ZXmOYLTLHSuBZIbv0I9JFUajQcFtqf/view?usp=drivesdk",
+
+            description:
+                "The last Sunday I was there... Trust me it was fun 😊"
+        },
+
+
+        {
+            id: "the-cards",
+            title: "The Cards",
+            type: "Video",
+            category: "Personal",
+
+            media:
+                "https://drive.google.com/file/d/13zz720AL6CCJgLOE9wVuhKwqTooFUTLg/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1JJhsMWU_lfsZYwS5J5m1ivHqEaNcu9oG/view?usp=drivesdk",
+
+            description:
+                "I was thinking about where I was to go all night to the point that I couldn't sleep but make this video"
+        },
+
+
+        {
+            id: "first-time-in-mavuno-church",
+            title: "First Time in Mavuno Church",
+            type: "Video",
+            category: "Mavuno Church",
+
+            media:
+                "https://drive.google.com/file/d/1ZI-ykEKyAFFKUMoxMXXuc2YWmXObekQS/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1U7KAFyogjp5ku613J4PAbesWrfHT-SdA/view?usp=drivesdk",
+
+            description:
+                "I was supper happy to be there"
+        },
+
+
+        {
+            id: "aurora-feed-structure",
+            title: "Aurora feed Structure",
+            type: "Video",
+            category: "Aurora Newdawn",
+
+            media:
+                "https://drive.google.com/file/d/1SAg1bDTEE2kKwmABoY5z5ceKwzqDddem/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1dOWacW0t8NFYrMoLZbScgVlQ8_LlXW2U/view?usp=drivesdk",
+
+            description:
+                "Let's say a bit rushed, but did my best"
+        },
+
+
+        {
+            id: "outreach-trailer",
+            title: "Outreach Trailer",
+            type: "Video",
+            category: "Mavuno Church",
+
+            media:
+                "https://drive.google.com/file/d/1BlJlIXJQTwNQQXM_jvc123X-j0uzVnb3/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1rsjuvfJrfYHTla3UwT7QmP6mGQFcKxP-/view?usp=drivesdk",
+
+            description:
+                "You ask me and I say this this is the best thing I ever made having very little... I think I have watched it so many times that compared to its view I can bet 50% is mine"
+        },
+
+
+        {
+            id: "war-short-version",
+            title: "War short version",
+            type: "Video",
+            category: "Mavuno Church",
+
+            media:
+                "https://drive.google.com/file/d/1KalZ_MZif812s0tt8FEKZQrdOM0jAy1m/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1U7KAFyogjp5ku613J4PAbesWrfHT-SdA/view?usp=drivesdk",
+
+            description:
+                "Just a happy one... The still guy who ain't dancing must be me"
+        },
+
+
+        {
+            id: "war",
+            title: "War",
+            type: "Video",
+            category: "Mavuno Church",
+
+            media:
+                "https://drive.google.com/file/d/1mJSOKfOPNuQpFz02JWO5NJKCuw9RZPhn/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1CbuAQEWUSdhRKkYtTVX8L7XQbUYo_NJ1/view?usp=drivesdk",
+
+            description:
+                "Must say I prefer the short one, although subsidiaries caused this... You argue with the sponsers"
+        },
+
+
+        {
+            id: "hello-class",
+            title: "Hello Class",
+            type: "Video",
+            category: "Side effects",
+
+            media:
+                "https://drive.google.com/file/d/1WmR9PWKHnLwwJ9kR0dyL_bn-QYHLM8Da/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1C83avzrnaB_zRcXtICTuOthSTLNltZQu/view?usp=drivesdk",
+
+            description:
+                "I must say I either didn't like the video material presented at my editors desk or didn't understand the requirements... It ain't my best... Deep regrets"
+        },
+
+
+        {
+            id: "vision-plus",
+            title: "Vision plus",
+            type: "Video",
+            category: "Vision",
+
+            media:
+                "https://drive.google.com/file/d/19uQbQCE7QnTACr0PbSseX81sCfIvHo5z/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/11grfvF-lUjEZKE3sCUGCFC-oa6YcREOE/view?usp=drivesdk",
+
+            description:
+                "Bro's dream"
+        },
+
+
+        {
+            id: "erin-and-aurora",
+            title: "Erin and Aurora",
+            type: "Video",
+            category: "Aurora Newdawn",
+
+            media:
+                "https://drive.google.com/file/d/1r8gsWTeqN7ad7HnV6GXdX875xN7JANj4/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1HVv0ufPVJPkvOHEzlTsbAdhjbSeL0LCP/view?usp=drivesdk",
+
+            description:
+                "Well I did that one professionally... Well before I started drafting the video I didn't expect it to turn this amazing"
+        },
+
+
+        {
+            id: "code-society",
+            title: "Code Society",
+            type: "Video",
+            category: "Aurora Newdawn",
+
+            media:
+                "https://drive.google.com/file/d/1qGIqiBW2_nsyvpZvXbBoQpfObfM1uI5n/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1ao5xmMVzbaZI_-wTxUzl7tVbRK6onByb/view?usp=drivesdk",
+
+            description:
+                "I love it just nice... This is among the few videos I have ever edited during the day... I at some point forgot I need to take a sip of tea which I must say it's not like me"
+        },
+
+
+        {
+            id: "emotional-wellness",
+            title: "Emotional Wellness",
+            type: "Video",
+            category: "Aurora Newdawn",
+
+            media:
+                "https://drive.google.com/file/d/1D6pDfIEbzmRODkw1RRgrcn_Npd7cdKi3/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1dOWacW0t8NFYrMoLZbScgVlQ8_LlXW2U/view?usp=drivesdk",
+
+            description:
+                "Well best AI voice ever"
+        },
+
+
+        {
+            id: "kisumu-mini-qati",
+            title: "KISUMU MINI-QATI",
+            type: "Video",
+            category: "Aurora Newdawn",
+
+            media:
+                "https://drive.google.com/file/d/19KdOIs87lpE_m9XaMbqrstkqIo3fYPGe/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1WkYSmWurDUgce-WoevTOYVjE11QqoO4k/view?usp=drivesdk",
+
+            description:
+                "Also great... Was liked with first draft.."
+        }
+
+    ],
+
+
+    /* -----------------------------------------------------
+       AUDIO
+       ----------------------------------------------------- */
+
+    audio: [
+
+        {
+            id: "anger",
+            title: "Anger",
+            type: "Podcast",
+            category: "podcast",
+
+            media: [
+                "https://drive.google.com/file/d/15AEOnaad8oNi8LbUgKGaLGQv9_MsdjCA/view?usp=drivesdk",
+                "https://drive.google.com/file/d/14W8qdLOYI2QhXcXBNGQM_3Wd9ubtC930/view?usp=drivesdk"
+            ],
+
+            thumbnail: "",
+
+            description:
+                "AI generated... But contents came from a blog I wrote..."
+        },
+
+
+        {
+            id: "gm-empire",
+            title: "GM empire",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1Q_v3xiVlh2Ws-QRm4cWH3KUzTRmSbg3c/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1VgMkgRs_mvpt1LL78zSRh7dSn0TaIz7_/view?usp=drivesdk",
+
+            description:
+                "Ai haux"
+        },
+
+
+        {
+            id: "gm-drill",
+            title: "GM drill",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1Klq3J4rqc0JbQHJjmhJfUexpaYqfbVfm/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1ZCRs0UdJO185Vds-pEoJk-CxMUglOzXW/view?usp=drivesdk",
+
+            description:
+                "Nice"
+        },
+
+
+        {
+            id: "gm-truths",
+            title: "GM truths",
+            type: "podcast",
+            category: "Nice",
+
+            media:
+                "https://drive.google.com/file/d/12-II_-JlxY_Z6Ef78YietmHl4cL5GNC0/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1Ian0xmTIGtxURZAoHoLWMCK8VCT6cG_e/view?usp=drivesdk",
+
+            description:
+                "Precise"
+        },
+
+
+        {
+            id: "future-in-making",
+            title: "Future in Making",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1DGXU_7hQrY3ZY_y-fVRnWjrFtAfrNsX7/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/14-o1YueV4zRj880cFpwtlDasZbktMDhN/view?usp=drivesdk",
+
+            description:
+                "I love this one"
+        },
+
+
+        {
+            id: "gm",
+            title: "GM",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/11ZCq-oq5kC7SsPfuQD8ii6NCKOcS8_kB/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/10MVtTefSqiJmmL9mikjHja_j4Ha7Hih0/view?usp=drivesdk",
+
+            description:
+                "Well"
+        },
+
+
+        {
+            id: "update-guys",
+            title: "Update guys",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1q8d21of8jM2i2oFVlv0NpguTu5Osv7Nk/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1iJYM7euryirrSecbCOLxIWjNS_iyvb0M/view?usp=drivesdk",
+
+            description:
+                "Nicky jam"
+        },
+
+
+        {
+            id: "well",
+            title: "Well",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1eWZPqAQgWYWTCq4o85I1vnlGibYVLzX7/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1gmg2pnI0ywfbqvZONugOF6w_rQopwfW6/view?usp=drivesdk",
+
+            description:
+                "Nice"
+        },
+
+
+        {
+            id: "a",
+            title: "A",
+            type: "Song",
+            category: "Me",
+
+            media:
+                "https://drive.google.com/file/d/1SEJ2xwuJJU0TFGQrWE1HERiQSL9Zisqb/view?usp=drivesdk",
+
+            thumbnail:
+                "https://drive.google.com/file/d/1VgMkgRs_mvpt1LL78zSRh7dSn0TaIz7_/view?usp=drivesdk",
+
+            description:
+                "last"
+        }
+
+    ],
+
+
+    /* -----------------------------------------------------
+       JOURNAL / BLOG
+       ----------------------------------------------------- */
+
+    journal: [
+
+        {
+            title: "The Power of Accountability in Church",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/the-power-of-accountability-in-church.html?m=1"
+        },
+
+        {
+            title: "The Unfolding Gift",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/the-unfolding-gift.html"
+        },
+
+        {
+            title: "How to Get Rich Quickly: Reality or Myth?",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/how-to-get-rich-quickly-reality-or-myth.html"
+        },
+
+        {
+            title: "Book",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/book.html"
+        },
+
+        {
+            title: "The Best Hotels in Kenya: Where Luxury...",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/the-best-hotels-in-kenya-where-luxury.html"
+        },
+
+        {
+            title: "The Battle for Ukraine's Leadership",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/the-battle-for-ukraines-leadership.html?m=1"
+        },
+
+        {
+            title: "World Leaders Agree to Solve Conflicts",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/world-leaders-agree-to-solve-conflicts.html"
+        },
+
+        {
+            title: "The African Entertainment Boom: How...",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/the-african-entertainment-boom-how.html"
+        },
+
+        {
+            title: "Scientists Confirm Your Phone Listens",
+            url:
+                "https://gmspecrum.blogspot.com/2025/03/scientists-confirm-your-phone-listens.html"
+        }
+
+    ]
+
+};
+
+
+/* ---------------------------------------------------------
+   MGDOWILL+ HELPER FUNCTIONS
+   --------------------------------------------------------- */
+
+/*
+   Return every media item in one array.
+
+   This makes it easier for the rest of the website to search
+   through videos, songs and podcasts without modifying the
+   original catalogue structure.
+*/
+function getAllMedia() {
+
+    const videos = Array.isArray(MGODWILL_CATALOGUE.videos)
+        ? MGODWILL_CATALOGUE.videos
+        : [];
+
+    const audio = Array.isArray(MGODWILL_CATALOGUE.audio)
+        ? MGODWILL_CATALOGUE.audio
+        : [];
+
+    return [...videos, ...audio];
+}
+
+
+/*
+   Find one media item by its unique ID.
+*/
+function getMediaById(id) {
+
+    if (!id) {
+        return null;
+    }
+
+    return getAllMedia().find(function(item) {
+        return item.id === id;
+    }) || null;
+}
+
+
+/*
+   Convert a Google Drive "view" URL into a Drive preview URL.
+
+   Example:
+   /file/d/ABC/view
+   becomes:
+   /file/d/ABC/preview
+*/
+function getDrivePreviewUrl(url) {
+
+    if (!url || typeof url !== "string") {
+        return "";
+    }
+
+    const match = url.match(/\/file\/d\/([^/]+)/);
+
+    if (!match) {
+        return url;
+    }
+
+    return "https://drive.google.com/file/d/" + match[1] + "/preview";
+}
+
+
+/*
+   Convert a Google Drive file URL into a thumbnail URL.
+
+   This is particularly useful for the thumbnails you prepared
+   for the catalogue.
+*/
+function getDriveThumbnailUrl(url) {
+
+    if (!url || typeof url !== "string") {
+        return "";
+    }
+
+    const match = url.match(/\/file\/d\/([^/]+)/);
+
+    if (!match) {
+        return url;
+    }
+
+    return "https://drive.google.com/thumbnail?id=" +
+        match[1] +
+        "&sz=w1200";
+}
+
+
+/*
+   Mavuno Church has one dedicated image.
+
+   Every item belonging to the Mavuno Church category therefore
+   receives the same thumbnail, regardless of the individual
+   thumbnail originally supplied.
+*/
+function getMediaThumbnail(item) {
+
+    if (!item) {
+        return "";
+    }
+
+    if (
+        typeof item.category === "string" &&
+        item.category.toLowerCase() === "mavuno church"
+    ) {
+        return getDriveThumbnailUrl(
+            MGODWILL_CATALOGUE.site.mavunoChurchImage
+        );
+    }
+
+    if (item.thumbnail) {
+        return getDriveThumbnailUrl(item.thumbnail);
+    }
+
+    return "";
+}
+
+
+/*
+   Return a media item's first playable URL.
+
+   The "Anger" podcast contains two Drive links. The player
+   system can later expose both rather than silently discarding
+   one of them.
+*/
+function getPrimaryMediaUrl(item) {
+
+    if (!item || !item.media) {
+        return "";
+    }
+
+    if (Array.isArray(item.media)) {
+        return item.media.length > 0
+            ? item.media[0]
+            : "";
+    }
+
+    return item.media;
+}
+
+
+/*
+   Return all media URLs belonging to an item.
+*/
+function getMediaUrls(item) {
+
+    if (!item || !item.media) {
+        return [];
+    }
+
+    if (Array.isArray(item.media)) {
+        return item.media.filter(function(url) {
+            return typeof url === "string" && url.trim() !== "";
+        });
+    }
+
+    return [item.media];
+}
+
+
+/*
+   Return media grouped by category.
+*/
+function getMediaCategories() {
+
+    const categories = [];
+
+    getAllMedia().forEach(function(item) {
+
+        if (
+            item &&
+            item.category &&
+            !categories.includes(item.category)
+        ) {
+            categories.push(item.category);
+        }
+
+    });
+
+    return categories;
+}
+
+
+/*
+   Search the complete media catalogue.
+
+   The search examines:
+   - title
+   - description
+   - category
+   - type
+*/
+function searchMedia(query) {
+
+    if (!query || typeof query !== "string") {
+        return getAllMedia();
+    }
+
+    const term = query.trim().toLowerCase();
+
+    if (!term) {
+        return getAllMedia();
+    }
+
+    return getAllMedia().filter(function(item) {
+
+        const searchableText = [
+            item.title,
+            item.description,
+            item.category,
+            item.type
+        ]
+        .filter(Boolean)
+        .join(" ")
+        .toLowerCase();
+
+        return searchableText.includes(term);
+    });
+}
